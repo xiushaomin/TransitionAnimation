c207b8f0cfbeec5fca342414e04df093c78091b3
xiushaomin <280244173@qq.com>
2016-07-12 22:36:35 +0800

    添加注释

--- TransitionAnimation/TransitionAnimation/PresentAnimationHelper.m
+++ TransitionAnimation/TransitionAnimation/PresentAnimationHelper.m
@@ -50,16 +50,21 @@
 }
 
 - (void) _doPresentAnimation:(id <UIViewControllerContextTransitioning>) transitionContext{
+    //拿到fromViewVc 也就是presentViewController
     UIViewController *fromViewVc = [transitionContext viewControllerForKey:UITransitionContextFromViewControllerKey];
+    //拿到toViewController 也就是presentedController
     UIViewController *toViewVc = [transitionContext viewControllerForKey:UITransitionContextToViewControllerKey];
+    //截屏present.view做动画的view
     UIView *animationView = [fromViewVc.view snapshotViewAfterScreenUpdates:NO];
     animationView.frame = fromViewVc.view.frame;
     fromViewVc.view.hidden = YES;
+    //需要做动画 必须加到这个容器的view
     UIView *containerView = [transitionContext containerView];
     [containerView addSubview:animationView];
     [containerView addSubview:toViewVc.view];
     
     toViewVc.view.frame = (CGRect) {0 , containerView.frame.size.height, containerView.frame.size.width, 300};
+    //开始动画了
     [UIView animateWithDuration:[self transitionDuration:transitionContext] delay:0 usingSpringWithDamping:0.55 initialSpringVelocity:1.0/0.55 options:0 animations:^{
         
         toViewVc.view.transform = CGAffineTransformMakeTranslation(0, - 300);
@@ -78,12 +83,14 @@
 }
 
 - (void)_doDismissAnimation:(id <UIViewControllerContextTransitioning>) transitionContext {
+    //fromView = presentedViewController
     UIViewController *fromViewVc = [transitionContext viewControllerForKey:UITransitionContextFromViewControllerKey];
     UIViewController *toViewVc = [transitionContext viewControllerForKey:UITransitionContextToViewControllerKey];
     UIView *containerView = [transitionContext containerView];
     NSArray *subviewsArray = containerView.subviews;
     UIView *animationView = subviewsArray[MIN(subviewsArray.count, MAX(0, subviewsArray.count - 2))];
     [UIView animateWithDuration:[self transitionDuration:transitionContext] animations:^{
+        //还原
         fromViewVc.view.transform = CGAffineTransformIdentity;
         animationView.transform = CGAffineTransformIdentity;
     } completion:^(BOOL finished) {
